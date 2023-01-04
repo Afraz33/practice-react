36bc5be939df512fa017784f59f8cf2e671f5de4
Afraz33 <afraz3301@gmail.com>
2023-01-04 23:21:07 +0500

    Vimdhayak

--- famzi-jamed/src/App.js
+++ famzi-jamed/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Vimdhayak dogesh dogelina
         </p>
         <a
           className="App-link"
