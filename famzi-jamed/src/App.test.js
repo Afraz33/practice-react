23a76b517228b72c98e86f7ad53e6f87cad6a00a
Afraz33 <afraz3301@gmail.com>
2023-01-04 23:26:44 +0500

    Test case added

--- famzi-jamed/src/App.test.js
+++ famzi-jamed/src/App.test.js
@@ -6,3 +6,10 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test1('Dogesh', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Dogelina/i);
+  expect(linkElement).toBeInTheDocument();
+});
